87bb5901c017c356caa23b47c15c4cadd2568aff
offlineflood <108521240+offlineflood@users.noreply.github.com>
2022-08-05 00:35:56 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # TheOksigen
-TOKEN = "5337183930:AAEQc8pbjleTSxzyjtS_GHYWfeVvcy4pEKo"
+TOKEN = "5534507453:AAHKub0_Xb70F6HdMWxaZZtkx7JcOg1rxZk"
 with open('soz.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
